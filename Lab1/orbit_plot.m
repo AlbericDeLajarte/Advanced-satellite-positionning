0096d38b92014e2a19459a052583b6a8603efd1e
StLebrun <43522333+StLebrun@users.noreply.github.com>
2020-02-28 16:25:44 +0100

    Add files via upload
    Update plot

--- Lab1/orbit_plot.m
+++ Lab1/orbit_plot.m
@@ -1,17 +1,19 @@
 function [] = orbit_plot()
 [t_oe, sqrt_a, e, M_0, time_span] = ephemerides();
-first = time_span(1);
-last = time_span(2);
-time_vect = [first:100:last];
+t_start = time_span(1);
+t_end = time_span(2);
+%time_vect = [t_start:100:t_end];
 
-X = zeros(length(time_vect));
-Y = zeros(length(time_vect));
-
+X = zeros(1, t_end- t_start);
+Y = zeros(1, t_end- t_start);
 i=1;
-for time = time_vect
-    [X(i), Y(i)] = sat_coordinates(time, t_oe, sqrt_a, e, M_0);
-    i=i+1;
+for t = t_start:t_end
+    [X(i), Y(i)] = sat_coordinates(t, t_oe, sqrt_a, e, M_0);
+    i = i+1;
 end
+figure(1);
+plot3(t_start:t_end,X,Y);
+ylabel('X coordinate');
+zlabel('Y coordinate');
+title('Movement of the satellite')
 plot(X,Y);
-xlabel('X coordinate');
-ylabel('Y coordinate')
