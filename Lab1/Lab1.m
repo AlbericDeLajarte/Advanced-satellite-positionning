4768dc11876d3b5955b3bdaa38e2c4988d6381f6
Albéric de lajarte <albericlajarte@gmail.com>
2020-02-28 17:30:52 +0100

    Finish Part A

--- Lab1/Lab1.m
+++ Lab1/Lab1.m
@@ -1,10 +1,16 @@
+%% Lab 1.A
+
+% Satellite Navigation Message
 t_oe = .225600000000E+06; % [seconds]
 a = (.544062105942E+04)^2; % [meters]
 e = .404827296734E-04;
 M_o = .194850072201E+01; % [radians]
 
-[x0, y0] = posGPS(225445, t_oe, a, e, M_o)
+% Position at one time
+[x0, y0] = posGPS(225445, t_oe, a, e, M_o);
 
+
+% Position in a time interval
 t_start = 174600;
 t_end = 225000;
 
@@ -15,9 +21,21 @@ for t = t_start:t_end
     [x(k), y(k)] = posGPS(t, t_oe, a, e, M_o);
     k = k+1;
 end
+
+% Apogee and Perigee
+apo = a*(1+e);
+peri = a*(1-e);
+
+% Plot
 figure(1);
 plot3(t_start:t_end,x,y);
 xlabel("Time [s]")
 ylabel("X coordinate [m]");
 zlabel("Y coordinate[m] ");
-title("Movement of the satellite")
+%title("Movement of the satellite")
+
+figure(2);
+plot(t_start:t_end, x, t_start:t_end, y);
+xlabel("Time [s]");
+ylabel("X/Y coordinate [m] ");
+legend("X position", "Y position");
