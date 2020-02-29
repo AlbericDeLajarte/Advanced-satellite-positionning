af6406962f049a2814239c0f287a6cc3076760da
StLebrun <43522333+StLebrun@users.noreply.github.com>
2020-02-29 21:21:51 +0100

    Values already in LabA.m

--- Lab1/ephemerides.m
+++
@@ -1,6 +0,0 @@
-function [t_oe, sqrt_a, e, M_0, time_span] = ephemerides()
-M_0 = .194850072201E+01; % [radians]
-e = .404827296734E-04 ;
-sqrt_a = .544062105942E+04; % [sqrt of meters]
-t_oe = .225600000000E+06; % [seconds]
-time_span =  [174600 225000]; 
