6d10918b05d8a9a12173e835486854ff072bb577
Stlebrun <stephanie.lebrun@outlook.com>
2020-03-06 16:38:19 +0100

    tellement éveillés

--- Lab2/Lab2A.m
+++ Lab2/Lab2A.m
@@ -2,7 +2,8 @@
 
 sat_nums1 = [2 5 8 12];
 sat_nums2 = [2 5 8 11 12 24 25];
-
-
+t = 225445.0; % Time of week [s]
+c =299792458; % [m/s]
+we = 7.2921151467e-5; %[m/s]
 
 % Find location of Galileo receiver's antenna using Bancroft algrithm
