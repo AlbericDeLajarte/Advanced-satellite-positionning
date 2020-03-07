b24255387b23adda34166f35523b19d7c37f69dc
Stlebrun <stephanie.lebrun@outlook.com>
2020-03-07 11:23:10 +0100

    push des notes

--- Lab2/bancroft.m
+++ Lab2/bancroft.m
@@ -3,5 +3,5 @@ function [] = bancroft(t, sat_nums, we)
 % Computation of ECI and ECEF coordinates of given satellites: 
 [eci_matrix, ecef_matrix] = eci_and_ecef_coordinates(t, sat_nums, we);
 
-% First iteration
-% Second iteration 
+%% Correction of pseudoranges
+
