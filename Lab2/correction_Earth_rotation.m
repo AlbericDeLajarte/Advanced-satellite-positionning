dc2b8b5cb12fe82ce4147a471cbbfeeb17afcea5
Albéric de lajarte <albericlajarte@gmail.com>
2020-03-08 15:39:10 +0100

    added fct for rotation and bancroft and lorentz product

---
+++ Lab2/correction_Earth_rotation.m
@@ -0,0 +1,16 @@
+% Correction of the satellite's coordinate to take into account the
+% rotation of the Earth during the transmission
+function X_corr = correction_Earth_rotation(X, delay)
+
+% Rotation angle in radian
+theta = delay*7.2921151467e-5;
+
+% Rotation matrix around the Z axis (axis perpendicular to the equator in ECEF coordiante)
+R = [cos(theta) -sin(theta) 0;
+     sin(theta)  cos(theta) 0;
+     0             0        1];
+ 
+
+X_corr = R*X;
+ 
+end
---
+++ lorentz_product.m
@@ -0,0 +1,7 @@
+function product = lorentz_product(g, h)
+M = eye(4,4);
+M(4,4) = -1;
+
+product = g'*M*h;
+
+end
