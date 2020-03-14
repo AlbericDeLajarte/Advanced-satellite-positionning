7bac7f4c4c97465da3dcf714dab3e0f7a619d176
Stlebrun <stephanie.lebrun@outlook.com>
2020-03-14 16:00:51 +0100

    times are fucked up

--- Lab2/bancroft_approach.m
+++ Lab2/bancroft_approach.m
@@ -2,15 +2,34 @@ function [] = bancroft_approach(t, sat_nums, we, pseudoranges)
 % Speed of light
 c = 299792458; %[m/s]
 
-%% Correction of pseudoranges with SV clock error dtk: 
+%% Correction of pseudoranges with SV clock error dtk:
+dtks = zeros(len(sat_nums));
+tis = zeros(len(sat_nums));
+taus = zeros(len(sat_nums));
+
+% To compute a dtk we need ti and toe
+% To compute ti we need propagation/travel time tau
+% First approximation of tis: 
+tis = pseudoranges/c;
+
+for i = 1:len(sat_nums)
+    af0 = ;
+    af1 = ;
+    af2 = ;
+    toe = ;
+    ti = ;
+    dtks(i) = af0+af1(tis(i)-toe)+ af2(tis(i)-toe)^2 ;
+end
+
 % Propagation time tau
 % ???
-% say for now:
-corrected_pseudoranges = values(pseudoranges);
+corrected_pseudoranges = values(pseudoranges) + dtks;
 
 %% Computation of ECI and ECEF coordinates of given satellites:
-% should we change t??? t = t - tau
-[~, ecef_matrix] = eci_and_ecef_coordinates(t, sat_nums, we);
+% we compute for time tk which is = reception time - transmission time 
+tk = ti - tau;
+[~, ecef_matrix] = eci_and_ecef_coordinates(tk, sat_nums, we);
+% ecef_matrix is a len(sat_nums) x 3 matrix
 
 %% Correction for Earth rotation: 
 worst_delay = 0.072; %s
@@ -34,7 +53,7 @@ if len(sat_nums) == 4
     %% Re-apply Bancroft:
     [r, b] = bancroft_4_Sat(corrected_ecef, corrected_pseudoranges');
 else
-    
+    % Same but with bancroft_overdetermined     
 end
 
 
