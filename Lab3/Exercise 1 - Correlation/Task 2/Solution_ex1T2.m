8baed2d54a124c2ef4db57c479bc98590708bf77
Stlebrun <stephanie.lebrun@outlook.com>
2020-03-20 15:16:30 +0100

    lab3 files

---
+++ Lab3/Exercise 1 - Correlation/Task 2/Solution_ex1T2.m
@@ -0,0 +1,5 @@
+%  Advanced Satellite Positioning - Lab 3 Task 2
+
+load('Ex1T2_mistery_signal.mat');
+
+
---
+++ Lab3/Exercise 2 - FFT Correlation/Solution_ex2.m
@@ -0,0 +1,4 @@
+% Advanced Satellite Positioning - Lab 3: Acquisition of GPS signal
+% Spring 2018
+
+load('Ex2_mistery_signal.mat');
