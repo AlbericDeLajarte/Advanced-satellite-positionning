9b0b80eaab123265d3b7b44c8b62567f4b50cc08
Albéric de lajarte <albericlajarte@gmail.com>
2020-03-24 10:43:12 +0100

    Added report files and finished task1 (EX1)

--- Lab3/Exercise 1 - Correlation/Task 1/SolutionEx1T1.m
+++ Lab3/Exercise 1 - Correlation/Task 1/SolutionEx1T1.m
@@ -1,12 +1,23 @@
 %  Advanced Satellite Positioning - Lab 3 Task 1
 
 % Step 1: Compute PRN codes using generateCAcode()
-
+CA_code_4 = generateCAcode(4);
+CA_code_12 = generateCAcode(12);
 
 
 % Step 2: Compute and plot circular auto correlation for PRN 12
 
-
+auto_corr_12 = computeCorrelation(CA_code_12, CA_code_12);
+figure(1);
+plot(0:length(auto_corr_12)-1, auto_corr_12);
+xlabel("Shift"); ylabel("correlation");
+title("Auto correlation of PRN number 12");
 
 
 % Step 3: Compute and plot circular cross correlation for PRN 12 and PRN 4
+
+cross_corr_12_4 = computeCorrelation(CA_code_12, CA_code_4);
+figure(2);
+plot(0:length(cross_corr_12_4)-1, cross_corr_12_4);
+xlabel("Shift"); ylabel("correlation");
+title("Cross correlation of PRN number 12 and 4");
--- Lab3/Exercise 1 - Correlation/Task 1/computeCorrelation.m
+++ Lab3/Exercise 1 - Correlation/Task 1/computeCorrelation.m
@@ -4,6 +4,17 @@
 
 function corr = computeCorrelation(signal_1, signal_2)
 
-
+if(length(signal_1) == length(signal_2) )
     
+    k = length(signal_1);
+else
+    k=0;
+    disp("Error: the two signals should have the same length");
+end
+
+corr = zeros(1, k);
+for i = 0:k-1
+    corr(i+1) = sum(signal_1.*circshift(signal_2, i));
+end
+
 end
