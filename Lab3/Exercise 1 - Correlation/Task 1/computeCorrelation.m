399606846978d527e022ffccddbdd00116bfa1f1
Albéric de lajarte <albericlajarte@gmail.com>
2020-03-26 10:33:40 +0100

    Added report files

--- Lab3/Exercise 1 - Correlation/Task 1/computeCorrelation.m
+++ Lab3/Exercise 1 - Correlation/Task 1/computeCorrelation.m
@@ -4,17 +4,34 @@
 
 function corr = computeCorrelation(signal_1, signal_2)
 
+% if(length(signal_1) >= length(signal_2) )
+%     
+%     k = length(signal_1);
+%     corr = zeros(1, k);
+%     signal_2 = [signal_2, zeros(1, length(signal_1)- length(signal_2))];
+%     for i = 0:k-1
+%         corr(i+1) = sum(signal_2.*circshift(signal_1, i));
+%     end
+% else
+%     k = length(signal_2);
+%     corr = zeros(1, k);
+%     signal_1 = [signal_1, zeros(1, length(signal_2)- length(signal_1))];
+%     for i = 0:k-1
+%         corr(i+1) = sum(signal_1.*circshift(signal_2, i));
+%     end
+% end
+
 if(length(signal_1) == length(signal_2) )
-    
     k = length(signal_1);
+    corr = zeros(1, k);
 else
-    k=0;
-    disp("Error: the two signals should have the same length");
+    k = 0;
+    disp("\nError: the signals should have the same length\n");
 end
 
-corr = zeros(1, k);
 for i = 0:k-1
     corr(i+1) = sum(signal_1.*circshift(signal_2, i));
 end
+    
 
 end
--- Lab3/Exercise 1 - Correlation/Task 2/Solution_ex1T2.m
+++ Lab3/Exercise 1 - Correlation/Task 2/Solution_ex1T2.m
@@ -1,5 +1,28 @@
 %  Advanced Satellite Positioning - Lab 3 Task 2
 
+%% Setup
+addpath("../Task 1");
 load('Ex1T2_mistery_signal.mat');
 
+fs = 6.5e6; % [Hz] 
+fc = 1.023e6; % [Hz] 
+data_length = 1; % [ms] 
 
+
+%% Compute the crosscorrelation with different PNR number
+maxPNR = zeros(1, 10);
+
+for PNR = 1:10
+    CA_code = generateGoldCodeSampled (PNR, fs, fc, data_length);
+    maxPNR(PNR) = max(computeCorrelation(CA_code, mistery_s));
+end
+
+    
+figure(1);
+plot(maxPNR)
+xlabel("Satellite number");
+ylabel("Correlation");
+
+CA_code_1 = generateCAcode(1);
+CA_code_ext_1 = interp1(0:1/(1.023e6):1e-3-1/(1.023e6), CA_code_1, 0:1/(6.5e6):1e-3-1/(6.5e6), 'nearest', 'extrap');
+plot(computeCorrelation(CA_code_ext_1, mistery_s));
